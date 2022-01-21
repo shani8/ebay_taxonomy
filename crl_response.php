1d90cdb9ab0fcc36a461dfe1f4c7de8918429ef2
Usman <shaniusman82@gmail.com>
2022-01-21 18:41:07 +0500

    add acces-tokken api in project

--- crl_response.php
+++ crl_response.php
@@ -1,41 +1,31 @@
 <?php
- 
- // A sample PHP Script to POST data using cURL
-  
-  $data = array(
-      'name' => 'tutsmake',
-      'email' => 'tutsmake@gmail.com',
-      'mobile' => '9898989898',
-  );
-    
-  $post_data = json_encode($data);
-    
-  // Prepare new cURL resource
-  $crl = curl_init('https://github.com/user/repo/main/db.json');
-  curl_setopt($crl, CURLOPT_RETURNTRANSFER, true);
-  curl_setopt($crl, CURLINFO_HEADER_OUT, true);
-  curl_setopt($crl, CURLOPT_POST, true);
+
+  $url = 'https://api.sandbox.ebay.com/identity/v1/oauth2/token';
+
+  $uname = "SaleemKh-Sqwigl-SBX-f500f24f5-471a9b31";
+  $pass = "SBX-500f24f593fa-3cd2-4ced-bf6a-9cf9";
+
+  $headers = array(
+    "cache-control: no-cache",
+    'application/x-www-form-urlencoded',
+    'Authorization: Basic '. base64_encode($uname . ":" . $pass) 
+  ); 
+
+  $post_data = 'grant_type=client_credentials&scope=https://api.ebay.com/oauth/api_scope';
+
+  $crl = curl_init($url);
+  curl_setopt($crl, CURLOPT_POST, 1);
   curl_setopt($crl, CURLOPT_POSTFIELDS, $post_data);
-    
-  // Set HTTP Header for POST request 
-  curl_setopt($crl, CURLOPT_HTTPHEADER, array(
-      'Content-Type: application/json',
-      'Content-Length: ' . strlen($payload))
-  );
-    
-  // Submit the POST request
+  curl_setopt($crl, CURLOPT_HTTPHEADER, $headers);
+  curl_setopt($crl, CURLOPT_RETURNTRANSFER, 1);
   $result = curl_exec($crl);
-    
-  // handle curl error
-  if ($result === false) {
-      // throw new Exception('Curl error: ' . curl_error($crl));
-      //print_r('Curl error: ' . curl_error($crl));
-      $result_noti = 0; die();
-  } else {
- 
-      $result_noti = 1; die();
-  }
-  // Close cURL session handle
-  curl_close($crl);
-    
+
+ $resultArray = json_decode($result, true);
+
+ $access_token = $resultArray["access_token"];
+ $expiry_seconds = $resultArray["expires_in"];
+ $token_type = $resultArray["token_type"];
+
 ?>
+
+
--- update_tokken.php
+++ update_tokken.php
@@ -1,24 +1,27 @@
 <?php
 include 'connection.php';
+include 'crl_response.php';
 
 $sql = "SELECT * FROM ebay_taxonomy_oauth";
 $result = mysqli_query($conn, $sql);
 $rec = mysqli_fetch_array($result);
-$random_string_tokken = substr(md5(mt_rand()), 0, 7);
 
+
+$random_string_tokken = $access_token;
 $currentDate = date('Y-m-d h:i:s');
-$currentDate = date('Y-m-d H:i:s',strtotime('+1 hour',strtotime($currentDate)));
+$currentDate = date('Y-m-d H:i:s',strtotime('+'.$expiry_seconds.' seconds',strtotime($currentDate)));
+
+// die($currentDate);
 
 if(mysqli_num_rows($result) > 0)
 {
 
-
-$sql = "UPDATE `ebay_taxonomy_oauth` SET `access_tokken`='".$random_string_tokken."',`expiration`='".$currentDate."' WHERE client_id = '0'"; 
+    $sql = "UPDATE `ebay_taxonomy_oauth` SET `access_tokken`='".$random_string_tokken."',`expiration`='".$currentDate."' WHERE client_id = '".$uname."'"; 
 }
 else
 {
 
-	$sql = "INSERT INTO `ebay_taxonomy_oauth` (`client_id`, `secret_key`, `access_tokken`, `expiration`) VALUES ('0','kjlieahjaldlal','".$random_string_tokken."','".$currentDate."')"; 
+	$sql = "INSERT INTO `ebay_taxonomy_oauth` (`client_id`, `secret_key`, `access_tokken`, `expiration`) VALUES ('".$uname."','".$pass."','".$random_string_tokken."','".$currentDate."')"; 
 }
 
 if($conn->query($sql))
@@ -27,7 +30,7 @@ if($conn->query($sql))
 }
 else
 {
-	echo false;
+   echo false;
 }
 
 
